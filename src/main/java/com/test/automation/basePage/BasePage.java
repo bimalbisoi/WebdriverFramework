e8f42f1648151ea481f74ff8cbb53afbb1103b1a
Bimalendu Bisoi <bimalendu.bisoi.contractor@move.com>
2020-05-02 17:40:03 +0530

    added the code for parameterization

--- src/main/java/com/test/automation/basePage/BasePage.java
+++ src/main/java/com/test/automation/basePage/BasePage.java
@@ -8,66 +8,73 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import com.test.automation.utils.Config;
 import com.test.automation.utils.WebEventListener;
 
 public class BasePage {
 
-	public static  WebDriver driver;
-	
+	public static WebDriver driver;
+
 	public static EventFiringWebDriver edriver;
 	public static WebEventListener eventListner;
+
 	/**
 	 * 
 	 * This method initialise the driver and return the driver instance.
 	 *
 	 * @return the driver
 	 */
-	public static WebDriver getDriver() 
-	
-	{
-		String browserName= "chrome";
-		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "/Users/bimalendu/chromedriver");
-			driver= new ChromeDriver();
-			}
+	public static WebDriver getDriver()
 
-		else if(browserName.equalsIgnoreCase("firefox")) {
-			driver=new FirefoxDriver();
+	{
+		/*
+		 * String browserName= Config.getConfigProperty("BROWSER_NAME"); => Here we read
+		 * the browser name from properties file Here we are passing it from command
+		 * prompt(using "mvn test -Dbrowser=chrome" ) instead of read the browser name
+		 * from properties file
+		 */
+		String browserName = System.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "/Users/bimalendu/chromedriver");
+			driver = new ChromeDriver();
 		}
-		else if(browserName.equalsIgnoreCase("IE")) {
-			System.setProperty("webdriver.ie.driver","/Users/bimalendu/IEDriver");
-			driver=new InternetExplorerDriver();
+
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("IE")) {
+			System.setProperty("webdriver.ie.driver", "/Users/bimalendu/IEDriver");
+			driver = new InternetExplorerDriver();
 		}
-		
-		edriver= new EventFiringWebDriver(driver);
-		//Creating the object of WebEventListener class (which defined under Utils package) to register it with EventFiringWebdriver
+
+		edriver = new EventFiringWebDriver(driver);
+		// Creating the object of WebEventListener class (which defined under Utils
+		// package) to register it with EventFiringWebdriver
 		eventListner = new WebEventListener();
 		edriver.register(eventListner);
-		driver=edriver;
-		
+		driver = edriver;
+
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
-		
+
 	}
-	
+
 	/**
 	 * This method opens a url
 	 *
 	 * @param url the url
 	 */
-	public void open(String url)
-	{
+	public void open(String url) {
 		driver.get(url);
 	}
-	
+
 	/**
 	 * This method Closes browser.
 	 */
-	public void close()
-	{
+	public void close() {
 		driver.quit();
 	}
 }
--- src/main/java/com/test/automation/dataProviders/LoginPageDataProvider.java
+++ src/main/java/com/test/automation/dataProviders/LoginPageDataProvider.java
@@ -6,6 +6,15 @@ import com.test.automation.utils.Config;
 
 public class LoginPageDataProvider {
 
+	/*
+	 * String loginPageUrl = Config.getConfigProperty("BASE_URL"); => Here we read
+	 * the url from properties file Here we are passing it from command prompt(using
+	 * "mvn test -DbaseUrl=http://www.webdriveruniversity.com/Contact-Us/contactus.html"
+	 * ) instead of read the url from properties file
+	 */
+
+	public static String baseUrl = System.getProperty("baseUrl");
+
 	/**
 	 * This DataProvider returns login page crdentials as well as the login page url
 	 *
@@ -13,7 +22,7 @@ public class LoginPageDataProvider {
 	 */
 	@DataProvider(name = "LoginPageCredentialsDataProvider")
 	public static Object[][] getLoginPageCrdentials() {
-		String loginPageUrl = Config.getConfigProperty("BASE_URL");
+		String loginPageUrl = baseUrl;
 		String firstName = Config.getConfigProperty("FIRST_NAME");
 		String lastName = Config.getConfigProperty("LAST_NAME");
 		String email = Config.getConfigProperty("EMAIL");
